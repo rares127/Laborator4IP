f1cbddd2426c3f10c29994a9baf2ecb530bd0928
iustin-cocea <161631136+iustin-cocea@users.noreply.github.com>
2025-03-20 19:27:58 +0200

    Update Laborator4IP.java

--- ClaseFaraAi/Laborator4IP.java
+++ ClaseFaraAi/Laborator4IP.java
@@ -1,16 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package com.mycompany.laborator4ip;
-
-/**
- *
- * @author RACU RARES LIVIU 
- */
 public class Laborator4IP {
 
     public static void main(String[] args) {
-        
+               Clasa clasa1 = new Clasa(
+                LocalDate.of(2025, 3, 20), 
+                "BD",        
+                "Seminar",               
+                profesor1,  
+                "C413",     
+                "Luni",    
+                "2B1"
+        );
+        Clasa clasa2 = new Clasa(
+                LocalDate.of(2025, 3, 21),
+                "SGBD",
+                "Curs",
+                profesor2,
+                "C401",
+                "Marti",
+                "2A4"
+        );
+
+        Clasa clasa3 = new Clasa(
+                LocalDate.of(2025, 3, 22),
+                "SD",
+                "Laborator",
+                profesor1,
+                "C412",
+                "Miercuri",
+                "1A2"
+        );
+
     }
 }
